923addbf14c35964f3b6b44df799b4cad7a76715
maximasprim <michaelmwasame6@gmail.com>
2024-06-18 22:39:53 +0300

    jsx

--- JSX-props/src/App.jsx
+++ JSX-props/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
   const name ="maximas"
   const moreDetails ={ 
     age:21,
@@ -18,6 +18,17 @@ function App() {
     <h2>Heyy,my name is { name }</h2>
     <p>My height is {moreDetails.height}</p>
     <p> {islogged ? " im logged in": "im not logged in"}</p>
+    <p> {moreDetails.age > 18 ? "im an adult" : "im a minor"}</p>
+    <p> my country is {moreDetails.country}</p>
+
+    <div>Favourite programming languages</div>
+
+    <ul style={ {listStyle:'none',backgroundColor: 'lightgreen'}}>
+      <li>Typescript</li>
+      <li>Javascript</li>
+      <li>C#</li>
+      <li>Python</li>
+    </ul>
     </>
   )
 }
