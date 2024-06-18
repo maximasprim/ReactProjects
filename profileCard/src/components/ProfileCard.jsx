9e23394fae46d46b5e89cc034d9cc16bb3167115
maximasprim <michaelmwasame6@gmail.com>
2024-06-18 21:36:50 +0300

    added profile cards

--- profileCard/src/components/ProfileCard.jsx
+++ profileCard/src/components/ProfileCard.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./profileCard.css";
 
-
-const ProfileCard = ({name,age,profilePicture,bio}) => {
+const ProfileCard = ({ name, age, profilePicture, bio }) => {
   return (
     <div className="main">
-    <div className="profile-card">
-      <img src={profilePicture} alt="no profile picture" className="profile-picture" />
-      <h2>Name: {name}</h2>
-      <p>Age: {age}</p>
-      <p>My Bio: {bio}</p>
-    </div>
+      <div className="profile-card">
+        <img
+          src={profilePicture}
+          alt="no profile picture"
+          className="profile-picture"
+        />
+        <h2 className="name"><span style={{color: "white"}}>Name:</span> {name}</h2>
+        <p>Age: {age}</p>
+        <p className="bio"><strong>My Bio:</strong> {bio}</p>
+      </div>
     </div>
   );
 };
