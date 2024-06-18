2f8b5999506c3ffb62387e37b186b424928884b8
maximasprim <michaelmwasame6@gmail.com>
2024-06-18 13:37:31 +0300

    firstapp,jsx-props

---
+++ JSX-props/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const name ="maximas"
+  return (
+    <h2>Heyy,my name is { name }</h2>
+  )
+}
+
+export default App
